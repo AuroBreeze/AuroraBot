f1a63267fa9fba7581b42766e2f8b0fd602c7d4f
AuroBreeze <1732373074@qq.com>
2025-07-17 17:29:41 +0800

    bat: 更新本地运行脚本
    - 移除了 local_run_once.bat 中的冗余操作
    - 添加了 .venv/Scripts/activate 调用到 main_run.bat
    - 简化了流程，提高了脚本的可读性和效率

--- local_run_once.bat
+++ local_run_once.bat
@@ -4,14 +4,6 @@ call .venv/Scripts/activate
 
 python -m pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
 
-pip install .
-
-python utils/Create_dirs.py
-
-pip install .
-
-python utils/DataMigrator.py
-
-pip install .
+pip install -e .
 
 python main.py
